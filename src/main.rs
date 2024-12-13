24eb569a035e9ca4e2ede1f44a854ff10a993597
har79 <github@harrycameron.com>
2024-12-13 23:54:31 +0000

    add initial src files

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::io;
+
+const GREETING: &str = "hello";
+
+fn r_fib(n: u32) -> u32 {
+    print!("#{n}: ");
+    let ret = if n <= 1 {
+        1
+    } else {
+        print!("(");
+        let mut ret = r_fib(n-1);
+        print!(" + ");
+        ret += r_fib(n-2);
+        print!(") => ");
+        ret
+    };
+    print!("{ret}");
+    ret
+}
+
+fn t_fib(n: u32) -> u32 {
+    fn t_fib_(n: u32) -> (u32, u32) {
+        let ret = if n == 0 {
+            print!("#0,-: ");
+            (1, 0)
+        } else {
+            print!("#{},{}: ", n, n-1);
+            print!("(");
+            let prev = t_fib_(n-1);
+            print!(") => ");
+            (prev.0 + prev.1, prev.0)
+        };
+        print!("{},{}", ret.0, ret.1);
+        ret
+    }
+
+    t_fib_(n).0
+}
+
+fn i_fib(n: u32) -> u32 {
+    let (mut c, mut p) = (1, 0);
+    print!("#0,-: {},{}", c, p);
+    for i in 1..=n {
+        (c, p) = (c + p, c);
+        print!(" => #{},{}: {},{}", i, i-1, c, p);
+    }
+    c
+}
+
+fn main() {
+    println!("{GREETING}");
+
+    println!("enter count");
+    
+    let mut count = String::new();
+    io::stdin().read_line(&mut count).expect("failed to read line");
+    let count: u32 = count.trim().parse().expect("failed to convert to int");
+
+    print!("recursive fib: ");
+    let result = r_fib(count);
+    println!(" => {result}");
+
+    print!("rec.tuple fib: ");
+    let result = t_fib(count);
+    println!(" => {result}");
+
+    print!("iterative fib: ");
+    let result = i_fib(count);
+    println!(" => {result}");
+}
